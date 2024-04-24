a8de3d41f61dd5a78ea231f8a2cb0ba5904f4a85
Taweechai Yuenyang <taweechai.yuenyang@outlook.com>
2024-04-24 10:12:01 +0700

    edit admin template

--- webbase/settings.py
+++ webbase/settings.py
@@ -63,7 +63,8 @@ ROOT_URLCONF = 'webbase.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # 'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
